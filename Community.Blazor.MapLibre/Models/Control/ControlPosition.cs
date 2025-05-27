5e3acd447807fb1f5cc3bb21f09da4b173be31f4
Lars Arvidsson <lars.arvidsson@gmail.com>
2025-05-27 14:52:58 +0200

    Fix serialization of ControlPosition

--- Community.Blazor.MapLibre/Models/Control/ControlPosition.cs
+++ Community.Blazor.MapLibre/Models/Control/ControlPosition.cs
@@ -5,6 +5,7 @@ namespace Community.Blazor.MapLibre.Models.Control;
 /// <summary>
 /// A position defintion for the control to be placed, can be in one of the corners of the map. When two or more controls are places in the same location they are stacked toward the center of the map.
 /// </summary>
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum ControlPosition
 {
     [JsonStringEnumMemberName("top-left")]
