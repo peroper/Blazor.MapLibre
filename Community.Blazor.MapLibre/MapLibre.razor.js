9d843e16211d41d1a80e236d151440b9520b9a3a
Lars Arvidsson <lars.arvidsson@gmail.com>
2025-10-13 12:42:12 +0200

    Fixed spelling of function "listImages"

--- Community.Blazor.MapLibre/MapLibre.razor.js
+++ Community.Blazor.MapLibre/MapLibre.razor.js
@@ -804,7 +804,7 @@ export function listens(container, type) {
  * @param {string} container - The map container.
  * @returns {string[]} A list of all image IDs.
  */
-export function istImages(container) {
+export function listImages(container) {
     return mapInstances[container].listImages();
 }
 
